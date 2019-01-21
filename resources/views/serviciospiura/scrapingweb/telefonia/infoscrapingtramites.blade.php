07ce2e0c063cc66f6ebb94e0bb6192075e2297a8
kevinQuispe <kquispe07@hotmail.com>
2019-01-21 18:48:05 -0500

    web scraoing

--- resources/views/serviciospiura/scrapingweb/telefonia/infoscrapingtramites.blade.php
+++ resources/views/serviciospiura/scrapingweb/telefonia/infoscrapingtramites.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.master')
 @section('title','Nueva')
 @section('content')
-    <h1>Tramites - Movistar</h1>
+
+    <div class="container">
+    <section>   
+              <h3><center>WEB SCRAPING CON TRAMITES-MOVISTAR <center></h3>
+              <?php
+                include_once('../app/phpdom/simple_html_dom.php');                   
+                // Create DOM from URL or file
+                $html = file_get_html('http://www.movistar.com.pe/');
+                $infomovistar= file_get_html ( 'http://www.movistar.com.pe/' ) -> plaintext ;
+                echo $infomovistar;           
+                ?>
+           </section>
+    
+    </div>
+       
 @endsection
