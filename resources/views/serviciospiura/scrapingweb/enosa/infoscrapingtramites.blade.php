b0fc2cb8b65e7a7b8dc723ad15172fcd6f4ff814
kevinQuispe <kquispe07@hotmail.com>
2019-01-29 17:52:59 -0500

    PDF INFORMACION WEB SCRAPING

--- resources/views/serviciospiura/scrapingweb/enosa/infoscrapingtramites.blade.php
+++ resources/views/serviciospiura/scrapingweb/enosa/infoscrapingtramites.blade.php
@@ -3,7 +3,22 @@
 @section('content')
 <div class="container">
 <section>
-<h3><center>WEB SCRAPING CON TRÁMITES-EPS ENOSA <center></h3>
+<h3><center>WEB SCRAPING INFORMACIÓN DE TRÁMITES-ENOSA <center></h3>
+<hr>
+<script>
+  function printDiv(divName) {
+     var printContents = document.getElementById(divName).innerHTML;
+     var originalContents = document.body.innerHTML;
+     document.body.innerHTML = printContents;
+     window.print();
+     document.body.innerHTML = originalContents;
+  }
+</script>
+<input class="btn btn-danger pull-right" type="button" onclick="printDiv('printableArea')" value="Guardar PDF" />
+    
+    <div id="printableArea">
+      <h3>Información de Trámites ENOSA</h3>
+      <hr>
 <?php
   include_once('../app/phpdom/simple_html_dom.php');                   
   // Create DOM from URL or file
@@ -610,4 +625,5 @@
   ?>
 </section>
 </div>
+<!-- fin del div para imprimir -->
 @endsection
